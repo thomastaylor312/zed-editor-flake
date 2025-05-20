3782a6d1e8c398be455d94b9e9b4a6c7222b20bd
thomastaylor312 <4294228+thomastaylor312@users.noreply.github.com>
2025-05-20 03:14:26 +0000

    chore: update Zed Editor to 0.186.10

--- packages/zed-editor/default.nix
+++ packages/zed-editor/default.nix
@@ -98,7 +98,7 @@ assert withGLES -> stdenv.hostPlatform.isLinux; let
 in
   rustPlatform.buildRustPackage (finalAttrs: {
     pname = "zed-editor";
-    version = "0.186.9";
+    version = "0.186.10";
 
     outputs =
       ["out"]
@@ -110,7 +110,7 @@ in
       owner = "zed-industries";
       repo = "zed";
       tag = "v${finalAttrs.version}";
-      hash = "sha256-z3QHD1EfJfhgLZwUv805RHCEKiBm1wCpJG1GAS9OJgE=";
+      hash = "sha256-QG6of8//P79WDOzT5eebP7CBHn6cSDKpDNd1mzlSFmM=";
     };
 
     patches = [
@@ -129,7 +129,7 @@ in
       '';
 
     useFetchCargoVendor = true;
-    cargoHash = "sha256-yWJ4lUdqsB9JBV2Tnz6Swjmrwoua+KdeN43W5bs52b4=";
+    cargoHash = "sha256-wPGM6C0ZSApgc+GarToYSCv6SsyHgw6eVi/t08SoNBI=";
 
     nativeBuildInputs =
       [
