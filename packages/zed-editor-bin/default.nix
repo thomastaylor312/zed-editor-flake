172b062e443fb4064ffaaeb6ea9f29816a3ccc24
thomastaylor312 <4294228+thomastaylor312@users.noreply.github.com>
2025-06-05 12:25:03 +0000

    chore: update Zed Editor to 0.189.5

--- packages/zed-editor-bin/default.nix
+++ packages/zed-editor-bin/default.nix
@@ -18,7 +18,7 @@
   testers,
   lib,
 }: let
-  version = "0.187.5";
+  version = "0.189.5";
 
   # Map from Nix system → { url, sha256, type }
   assets = {
@@ -26,28 +26,28 @@
       url =
         "https://github.com/zed-industries/zed/releases/download/"
         + "v${version}/zed-linux-x86_64.tar.gz";
-      sha256 = "sha256-5RBSlOhkGOQsoyJa58i7WOIgrh/xD2Vy6sJYmhGi9tg=";
+      sha256 = "sha256-aXZTHRUJ3kAokbYhEqEuCzjpFV0A7Zjasgp7qnm0H8s=";
       type = "tar.gz";
     };
     "aarch64-linux" = {
       url =
         "https://github.com/zed-industries/zed/releases/download/"
         + "v${version}/zed-linux-aarch64.tar.gz";
-      sha256 = "sha256-+hTj9aL1Ro7+cuJnpFLwUx5LakQ5NZcdMTsRIE5Dj54=";
+      sha256 = "sha256-8dxTwDCOe5RINM0zb3Q1UD1+DSE080F0iFadlOIgzSE=";
       type = "tar.gz";
     };
     "x86_64-darwin" = {
       url =
         "https://github.com/zed-industries/zed/releases/download/"
         + "v${version}/Zed-x86_64.dmg";
-      sha256 = "sha256-zttdHoweHY3PP5vD+XBVSGy5r/OG1guFYg8ODb0vXxo=";
+      sha256 = "sha256-B6UA/ua44/UGk/Y6SjebO1w/J9ZR0y4c+WpslQMYVko=";
       type = "dmg";
     };
     "aarch64-darwin" = {
       url =
         "https://github.com/zed-industries/zed/releases/download/"
         + "v${version}/Zed-aarch64.dmg";
-      sha256 = "sha256-uSx1jjw5ZEqvGyo4plrVmbvcoFZeTFo/wf+iEXRkDq4=";
+      sha256 = "sha256-jqWcVzE5KqxCXljXrljdvsPMzljWyvyxbdzi07+QhVA=";
       type = "dmg";
     };
   };
